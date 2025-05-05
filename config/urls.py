d3332457e364df0e28a368c9e3678dba0186382c
JamesWrightSDGKU <James.Wright@sdgku.edu>
2025-05-05 16:39:17 -0700

    FSDI 112 Final Report - Interim

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
+    path("accounts/", include ("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("posts/", include("posts.urls")),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+    ListView
+)
+from .models import Post
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = [
+        "title", "subtitle", "body", "status", "author"
+    ]
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = [
+        "title", "subtitle", "body", "status"
+    ]
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
